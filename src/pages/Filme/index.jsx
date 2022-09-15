87d71c9b047f99e5014e71bb5bd026478b266789
JoshyyDev <jrcbr321@gmail.com>
2022-09-15 11:51:41 -0300

    Configurando botão trailer

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './filme-info.css';
 import api from '../../services/api';
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
+
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true)
 
@@ -22,6 +24,8 @@ function Filme() {
             })
             .catch(() => {
                 console.log("FILME NAO ENCONTRADO")
+                navigate("/", { replace: true })
+                return;
             })
         }
 
@@ -31,7 +35,7 @@ function Filme() {
         return () => {
             console.log("COMPONENTE FOI DESMONTADO")
         }
-    }, [])
+    }, [navigate, id])
 
     if(loading) {
         return (
@@ -54,7 +58,7 @@ function Filme() {
             <div className='area-buttons'>
                 <button>Salvar</button>
                 <button>
-                    <a href="#">
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
